2d02c7eb89688a239a42e89075caebd4c9f4d800
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-05 21:36:42 -0500

    Creando el servicio getUsers en donde se manda a invocar el método getUsers del controlador

--- revistas_api/src/users/users.controller.ts
+++ revistas_api/src/users/users.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private userService: UsersService){}
 
+    @Get()
+    getUsers(): Promise<User[]>{
+        return this.userService.getUsers();
+    }
+
     @Post()
-    createUser(@Body() newUser: CreateUserDTO){
+    createUser(@Body() newUser: CreateUserDTO): Promise<User>{
         return this.userService.createUser(newUser);
     }
 
