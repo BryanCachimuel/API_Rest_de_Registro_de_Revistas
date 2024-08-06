b15ba8a438fb3c820fc638b20cac9b9712a6ad5a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-05 21:20:24 -0500

    Creando un constructor donde se manda a llamar al userService y se crea también un método createUser para crear un nuevo usuario

--- revistas_api/src/users/users.controller.ts
+++ revistas_api/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService: UsersService){}
+
+    @Post()
+    createUser(@Body() newUser: CreateUserDTO){
+        return this.userService.createUser(newUser);
+    }
+
+}
