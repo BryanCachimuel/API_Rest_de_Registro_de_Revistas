c375fbcf885f392693e08f558df1a3db5180167c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-09 23:09:50 -0500

    Creando el método createProfile que mediante la relación entre usuarios y perfil se puede realizar la creación de los perfiles de los usuarios

--- revistas_api/src/users/users.service.ts
+++ revistas_api/src/users/users.service.ts
@@ -4,22 +4,24 @@ import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UpdateUserDTO } from './dto/update-user.dto';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { Profile } from './profile.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
+    @InjectRepository(Profile) private profileRepository: Repository<Profile>,
   ) {}
 
   async createUser(user: CreateUserDTO) {
-
     const userFound = await this.userRepository.findOne({
       where: {
-        username: user.username
-      }
+        username: user.username,
+      },
     });
 
-    if(userFound){
+    if (userFound) {
       return new HttpException('El Usuario ya existe', HttpStatus.CONFLICT);
     }
 
@@ -34,30 +36,44 @@ export class UsersService {
   async getUser(id: number) {
     const userFound = await this.userRepository.findOne({ where: { id } });
 
-    if(!userFound){
+    if (!userFound) {
       return new HttpException('Usuario no Encontrado', HttpStatus.NOT_FOUND);
     }
 
     return userFound;
   }
 
-  async deleteUser(id: number){
-    const result = await this.userRepository.delete({id});
+  async deleteUser(id: number) {
+    const result = await this.userRepository.delete({ id });
 
-    if(result.affected === 0){
+    if (result.affected === 0) {
       return new HttpException('Usuario no Encontrado', HttpStatus.NOT_FOUND);
     }
     return result;
   }
 
-  async updateUser(id: number, user: UpdateUserDTO){
-    const userFound = await this.userRepository.findOne({ where: {id} });
+  async updateUser(id: number, user: UpdateUserDTO) {
+    const userFound = await this.userRepository.findOne({ where: { id } });
 
-    if(!userFound){
+    if (!userFound) {
       return new HttpException('Usuario no Encontrado', HttpStatus.NOT_FOUND);
     }
     const updateUser = Object.assign(userFound, user);
     return this.userRepository.save(updateUser);
   }
 
+  async createProfile(id: number, profile: CreateProfileDto) {
+    const userFound = await this.userRepository.findOne({ where: { id } });
+    if(!userFound){
+      return new HttpException('Usuario no Encontrado', HttpStatus.NOT_FOUND);
+    }
+    
+    const newProfile = this.profileRepository.create(profile);
+
+    const savedProfile = await this.profileRepository.save(newProfile);
+
+    userFound.profile = savedProfile;
+
+    return this.userRepository.save(userFound);
+  }
 }
