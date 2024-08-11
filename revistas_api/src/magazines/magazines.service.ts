184350ff084779b025bf52191209cfbbbfff66ee
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-10 21:43:14 -0500

    Corrigiendo el nombre de los arhivos y redireccionando las rutas con los nombres adecuados de los archivos para magazines

---
+++ revistas_api/src/magazines/magazines.service.ts
@@ -0,0 +1,28 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { Magazine } from './magazine.entity';
+import { Repository } from 'typeorm';
+import { CreateMagazineDto } from './dto/create-magazine.dto';
+
+@Injectable()
+export class MagazinesService {
+
+    constructor(
+        @InjectRepository(Magazine) private magazinesRepository: Repository<Magazine>,
+        private usersService: UsersService
+    ){}
+
+    async createMagazine(magazine: CreateMagazineDto) {
+        const userFound = await this.usersService.getUser(magazine.authorId);
+        if(!userFound) return new HttpException('Usuario No Encontrado', HttpStatus.NOT_FOUND);
+
+        const newMagazine = this.magazinesRepository.create(magazine);
+        return this.magazinesRepository.save(newMagazine);
+    }
+
+    getMagazine() {
+        return this.magazinesRepository.find();
+    }
+
+}
