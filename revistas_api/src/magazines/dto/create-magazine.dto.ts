f015998131a36b49d5455a95501eca6de16be68d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-10 21:35:30 -0500

    Creando la clase CreateMagazineDto donde se determinan los atributos de la entidad Magazine con sus respectivos tipos de datos

---
+++ revistas_api/src/magazines/dto/create-magazine.dto.ts
@@ -0,0 +1,9 @@
+export class CreateMagazineDto { 
+    yearPublication: number;
+    title: string;
+    description: string;
+    numberPages: number;
+    stock: number;
+    price: number;
+    authorId: number;
+}
