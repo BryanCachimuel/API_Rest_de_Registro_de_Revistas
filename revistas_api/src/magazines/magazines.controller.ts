e398f944294ba97b25ac451f9a13a6b874a92188
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-11 22:36:04 -0500

    Agregando los métodos createPost y getMagazine cons respectivos decoradores y consumo de de métodos de servicio

--- revistas_api/src/magazines/magazines.controller.ts
+++ revistas_api/src/magazines/magazines.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateMagazineDto } from './dto/create-magazine.dto';
+import { MagazinesService } from './magazines.service';
 
 @Controller('magazines')
-export class MagazinesController {}
+export class MagazinesController {
+
+    constructor(
+        private magazinesService: MagazinesService
+    ){}
+
+    @Post()
+    createPost(@Body() magazine: CreateMagazineDto ) {
+        return this.magazinesService.createMagazine(magazine);
+    }
+
+    @Get()
+    getMagazine() {
+        return this.magazinesService.getMagazines();
+    }
+
+}
