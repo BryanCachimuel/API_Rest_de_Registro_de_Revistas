d9338f2fd378bf0d5dff17387845179a6c63d8da
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-11 22:31:30 -0500

    Importando el modulo de UsersModule para colocarlo dentro del objeto Module en el apartado de imports

--- revistas_api/src/magazines/magazines.module.ts
+++ revistas_api/src/magazines/magazines.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MagazinesService } from './magazines.service';
+import { MagazinesController } from './magazines.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Magazine } from './magazine.entity';
+import { UsersModule } from 'src/users/users.module';
 
-@Module({})
-export class MegazinesModule {}
+@Module({
+  imports: [TypeOrmModule.forFeature([Magazine]), UsersModule],
+  providers: [MagazinesService],
+  controllers: [MagazinesController],
+})
+export class MagazinesModule {}
