17ca0d5bd59080070f8a3cd0ae20214b4d576919
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-15 22:03:05 -0500

    Creando la clase UpdateMagazineDto con los atributos de la entidad Magazine que pueden ser editados en lo que respecta a su contenido

---
+++ revistas_api/src/magazines/dto/update-magazine.dto.ts
@@ -0,0 +1,9 @@
+export class UpdateMagazineDto {
+    yearPublication?: number;
+    title?: string;
+    description?: string;
+    numberPages?: number;
+    stock?: number;
+    price?: number;
+    authorId?: number;
+}
