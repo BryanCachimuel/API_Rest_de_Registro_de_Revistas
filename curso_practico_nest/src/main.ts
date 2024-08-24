a385f536777ea299a97604fc5809d6548e3a5b90
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-08-23 22:55:40 -0500

    Configurando las dependicias como: morgan, setGlobalPrefix y la variable de entorno para el puerto de ejecución del proyecto

--- curso_practico_nest/src/main.ts
+++ curso_practico_nest/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors(CORS);
+
+  /*TODO: en la url se coloca el termino api: http://localhost:8000/api/users/say-hello */
+  app.setGlobalPrefix('api');
+
+  await app.listen(configService.get('PORT'));
+
+  console.log(`Aplicación ejecutandose en : ${await app.getUrl()}`);
 }
 bootstrap();
