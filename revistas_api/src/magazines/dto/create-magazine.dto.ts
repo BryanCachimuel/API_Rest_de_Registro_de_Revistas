e948e9d823ba9dd2baf35f919d39778835c5ad7c
Bryan_Cachimuel <bryanloyo56@gmail.com>
2024-12-30 21:51:26 -0500

    Eliminando los archivos dto para usuarios y revistas del api de revistas

--- revistas_api/src/magazines/dto/create-magazine.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateMagazineDto { 
-    yearPublication: number;
-    title: string;
-    description: string;
-    numberPages: number;
-    stock: number;
-    price: number;
-    authorId: number;
-}
--- revistas_api/src/magazines/dto/update-magazine.dto.ts
+++
@@ -1,9 +0,0 @@
-export class UpdateMagazineDto {
-    yearPublication?: number;
-    title?: string;
-    description?: string;
-    numberPages?: number;
-    stock?: number;
-    price?: number;
-    authorId?: number;
-}
