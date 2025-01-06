9bcf6a6deb05430aa5e874b93b0564ff7f7cfa19
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-05 22:12:48 -0500

    Creando en la clase RevistasService el método getRevistas para obtener todas las revistas registradas

--- api_rest_revistas/src/revistas/revistas.service.ts
+++ api_rest_revistas/src/revistas/revistas.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -6,5 +6,13 @@ export class RevistasService {
     
     constructor(private prisma: PrismaService){}
 
-    
+    async getRevistas(){
+        try {
+            const revistas = await this.prisma.revista.findMany()
+            return revistas;
+        } catch (error) {
+            if(error instanceof Error)
+                throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
 }
