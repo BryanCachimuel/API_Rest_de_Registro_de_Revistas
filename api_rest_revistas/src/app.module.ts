1f410f8f25e0064a861de07d41ed322aaa457af7
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-05 22:02:38 -0500

    Con la creación de los archivos del directorio de revistas se agrega automaticamente el import de RevistaModule dentro del @Module

--- api_rest_revistas/src/app.module.ts
+++ api_rest_revistas/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RevistasModule } from './revistas/revistas.module';
 
 @Module({
-  imports: [],
+  imports: [RevistasModule],
   controllers: [AppController],
   providers: [AppService],
 })
