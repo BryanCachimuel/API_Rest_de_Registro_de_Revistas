5450ea7888f968af9f2a3679dd8afec997245c44
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-06 18:52:24 -0500

    Creando el método getRevista para obtener una revista mediante un id dentro del proceso de servicio

--- api_rest_revistas/src/revistas/revistas.service.ts
+++ api_rest_revistas/src/revistas/revistas.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
@@ -11,6 +11,23 @@ export class RevistasService {
             const revistas = await this.prisma.revista.findMany()
             return revistas;
         } catch (error) {
+            if(error instanceof Error)
+                throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    async getRevista(id:number){
+        try {
+            const revista = await this.prisma.revista.findFirst({
+                where:{
+                    id
+                }
+            })
+            if(revista) return revista;
+            throw new NotFoundException(`La revista con el id ${id} no fue encontrada`)
+        } catch (error) {
+            if(error instanceof NotFoundException)
+                throw new NotFoundException(error.message)
             if(error instanceof Error)
                 throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
