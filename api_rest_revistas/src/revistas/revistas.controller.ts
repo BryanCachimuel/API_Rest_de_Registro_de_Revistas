e0d01f194f4cc4e34821afe57ca89dad4ef8c813
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-06 20:21:46 -0500

    Creando un método createRevista el cual tiene dos parámetros @Body y data que contiene la estructura de validación de los atributos, en esté método se realiza el consumo del método createRevista determinado en la clase RevistaService

--- api_rest_revistas/src/revistas/revistas.controller.ts
+++ api_rest_revistas/src/revistas/revistas.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Body } from '@nestjs/common';
 import { RevistasService } from './revistas.service';
+import { CreateRevistaDto } from './dto/create-revista.dto';
 
 @Controller('revistas')
 export class RevistasController {
@@ -14,6 +15,11 @@ export class RevistasController {
     @Get('/:id')
     async getRevista(@Param('id', ParseIntPipe) id: number){
         return await this.revistasService.getRevista(id)
+    }
+
+    @Post()
+    async createRevista(@Body() data:CreateRevistaDto){
+        return await this.revistasService.createRevista(data);
     }
 
 }
